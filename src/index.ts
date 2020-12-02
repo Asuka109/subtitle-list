75101cd5268f3fe512511b6801ad72594ec6c4d8
Asuka109 <1097198892@qq.com>
2020-12-02 20:59:05 +0800

    fix: 修复类型定义文件的导入问题

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import useSubtitles from './hooks/useSubtitles'
 import SubtitleList from './commons/SubtitleList'
+export * from './hooks/useSubtitles'
+export * from './commons/SubtitleList'
 
 export {
     useSubtitles,
